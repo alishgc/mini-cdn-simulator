7eab08126b62edd9221fed597047bd65a3bc2a82
alishgc <alishgc63@gmail.com>
2026-07-24 21:37:51 +0545

    implement edge proxy server with in-memory caching

--- edge/server.js
+++ edge/server.js
@@ -4,17 +4,39 @@ const PORT = process.env.PORT || 3001;
 
 const axios = require("axios")
 
+const originUrl = "http://localhost:3000"
+
+const cache = new Map();
+
 app.get("/{*splat}", async (req, res) => {
+
     // console.log(req.url)
     // console.log(req.params)
 
-    const response = await axios.get("http://localhost:3000"+ req.url)
+    // const response = await axios.get("http://localhost:3000"+ req.url)
 
-    console.log(Object.keys(response));
-    console.log(response)
+    // console.log(Object.keys(response));
+    // console.log(response)
 
 
-    res.send(response.data)
+    const isCached = cache.has(req.url)
+
+    if (!isCached) {
+      const response = await axios.get(originUrl + req.url)
+
+      console.log (`[EDGE] Cache MISS: ${req.url}`)
+
+      cache.set(req.url, response.data)
+      
+      res.send(response.data); 
+    }
+    
+    else {
+      console.log(`[EDGE] Cache HIT: ${req.url}`)
+
+      res.send(cache.get(req.url))
+
+    }
   
 })
 
--- origin/server.js
+++ origin/server.js
@@ -4,9 +4,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"))
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+app.use((req, res, next) => {
+    console.log(`[ORIGIN] ${req.method} ${req.url}`);
+    next();
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>Hello From origin to About page!</h1>')
 })
+
 
 app.listen(PORT, () => {
   console.log(`Origin Server running on http://localhost:3000`)
